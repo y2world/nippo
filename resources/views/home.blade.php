72c45f4acd10b7587db815d5cd269fa43a93ce28
y2world <yui92496@gmail.com>
2021-01-09 21:38:31 +0900

    Ver 2.2.1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('NIPPO', 'NIPPO') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'NIPPO') }}
+                    {{ config('NIPPO', 'NIPPO') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -73,9 +73,6 @@
         </nav>
 
         <main class="py-4">
-        <?php
-
-?>
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -102,12 +99,12 @@ width:700px;
    <form method="post" >  
      <div>
          <p>学習日</p>
-         <input type="date">
+         <input type="date" name="date">
      </div> 
      <br>
      <div class="contents">
        <p>学習内容：</p>
-       <textarea style="width:40%; height:100px;" placeholder="「躓いたこと」「できるようになったこと」など"></textarea> 
+       <textarea style="width:60%; height:100px;" placeholder="「躓いたこと」「できるようになったこと」など"></textarea> 
      </div><br>
 
      <div class="time">
@@ -150,11 +147,8 @@ width:700px;
  
     
    <hr>
-   
-   
-   
-  
-  </body>
+
+   </body>
 </html>
     
 
