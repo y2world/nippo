726226eb2586244a9f0c61fd5ad25a43dbaf4f3e
y2world <yui92496@gmail.com>
2021-01-11 23:04:29 +0900

    Ver 2.3.5

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
+    protected $table = 'posts';
     protected $fillable = ['content'];
 }
--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $table = 'posts'; // ここのテーブルの名前を間違えないように！
+    protected $fillable = ['date', 'contents'];
 }
