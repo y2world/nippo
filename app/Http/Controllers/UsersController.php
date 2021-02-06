4a0d65576f1c18191fb01ba93b4d4c328eec0d3d
y2world <yui92496@gmail.com>
2021-02-06 19:28:06 +0900

    Ver 2.7

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -50,7 +50,6 @@ class UsersController extends Controller
         $user = User::find($user->id); //idが、リクエストされた$userのidと一致するuserを取得
         $posts = Post::where('user_id', $user->id) //$userによる投稿を取得
             ->orderBy('created_at', 'desc') // 投稿作成日が新しい順に並べる
-            ->paginate(10); // ページネーション; 
         return view('users.show', [
             'user_name' => $user->name, // $user名をviewへ渡す
             'posts' => $posts, // $userの書いた記事をviewへ渡す
